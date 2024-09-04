af3f03c9a43ebec7b43005e873fe1c9e4d4ad8b2
harimtim <harimtim@outlook.de>
2024-09-04 22:49:57 +0200

    screen updated

--- lib/img.py
+++ lib/img.py
@@ -43,3 +43,25 @@ def create_vyse(wifi:bool, filename:str = "vyse.bmp"):
     img.paste(wifi, (228, 3), wifi)
 
     img.save(f"./img/{filename}")
+
+def create_vyseV2(wifi:bool, filename:str = "vyse.bmp"):
+    img = Image.new("RGB", (WIDTH, HEIGHT), "white")
+    draw = ImageDraw.Draw(img)
+    font1 = ImageFont.truetype("vyse.ttf", 60)
+    font2 = ImageFont.truetype("vyse.ttf", 12)
+    font3 = ImageFont.truetype("vyse.ttf", 10)
+
+    draw.text((WIDTH/2, HEIGHT/2-10), "Vyse", font=font1, fill="black", anchor="mm")
+    draw.text((WIDTH/2, HEIGHT/2+30), "made by harimtim", font=font2, fill="black", anchor="mm")
+    draw.text((5, 5), time.strftime("%T")+" Uhr", font=font3, fill="black")
+    draw.text((245, 117), "BETA", font=font3, fill="black", anchor="rb")
+
+    if wifi == True:
+        bmp = "check.bmp"
+    else:
+        bmp = "warning.bmp"
+
+    wifi = Image.open(f"./img/{bmp}").resize(size=(16, 16)).convert("RGBA")
+    img.paste(wifi, (228, 3), wifi)
+
+    img.save(f"./img/{filename}")
